5a7c9885549a36e1e49a98cffeac472eaff690ee
vagrant <vagrant@vagrant.vm>
2018-01-17 20:56:40 +0000

    Added function comments to illustrate how getGeocodeLocation works

--- geocode.py
+++ geocode.py
@@ -1,7 +1,20 @@
 import httplib2
 import json
-
 def getGeocodeLocation(inputString):
+    '''
+    This is a custom-made module named geocode. We import getGeocodeLocation
+    from geocode in findARestaurant.py. Once we pass in an input string, the
+    function replaces the spaces (" ") with a plus sign ("+"). This is a
+    crucial format for getting a proper HTTP 200 GET request. Once we
+    receive a proper get-request, we load the result in json, and parse
+    two parameters from the Google Maps API, which are the longitude
+    and latitude of the city in question. We use these parsed variables
+    to input into our Foursquare API GET request, found in findARestaurant.py
+    '''
+    # the Google API key is intended for my use only. If you are interested
+    # in working with this project, you can request your own API key from
+    # Google on developers.google.com. Thank you for understanding
+
     google_api_key = "AIzaSyA-pnTIuJqwtfsInE9n1slQictD0FdJjNI"
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (locationString, google_api_key))
