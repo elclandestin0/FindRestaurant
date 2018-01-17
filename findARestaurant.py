6686369805721f6c63c5f9a82af7aa384a44c127
vagrant <vagrant@vagrant.vm>
2018-01-17 21:05:03 +0000

    Added function comments to illustrate how findARestaurant works

--- findARestaurant.py
+++ findARestaurant.py
@@ -7,23 +7,29 @@ import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
+# The Foursquare client ID and client secret are intended
+# for my use only if you are interested, you can get your
+# own API key @https://developer.foursquare.com
+
 foursquare_client_id = "PVYC4HWHSBIQWWKH4TKWR2UXGR5FVWKNIUEVKOKQVGM0BNFM"
 foursquare_client_secret = "IOOVRHSXQGICSDPULID0VPN23D35TFASEKBKJWLZKHAGMSYF"
 
 
-def findARestaurant(mealType,location):
-	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
-	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
-	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
-	#3. Grab the first restaurant
-	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-	#5. Grab the first image
-	#6. If no image is available, insert default a image url
-	#7. Return a dictionary containing the restaurant name, address, and image url
+def findARestaurant(mealType, location):
+    '''
+    The function takes in the returned variables from getGeocodeLocation
+    and inputs it into the url variable. It also inputs the client_id,
+    client_secret and mealType to successfully query the list of venues,
+    serialized into JSON. From there, we want to find the first restaurant
+    in the search query. In doing so, we parse the venue_id, restaurant_name,
+    and restaurant_address. Next, we plug it into a restaurant dictionary
+    and return the results to the user.
+    '''
     latitude, longitude = getGeocodeLocation(location)
     url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s' % (foursquare_client_id, foursquare_client_secret, latitude, longitude, mealType))
     h = httplib2.Http()
     result = json.loads(h.request(url,'GET')[1])
+    # Retrieve the first restaurant from the search query.
     restaurant = result['response']['venues'][0]
     venue_id = restaurant['id']
     restaurant_name = restaurant['name']
@@ -34,16 +40,6 @@ def findARestaurant(mealType,location):
         "address": restaurant_address
     }
     print restaurant_dict
-
-    #print restaurant_name
-    #print restaurant_address
-    # for row in result:
-    #     print row
-    # # print result
-    # return result
-    # url_venue = ('https://api.foursquare.com/v2/venues/search')
-    # return result
-    # return restaurant
 
 findARestaurant('Burger','Amsterdam')
 # findARestaurant("Tea", "Amsterdam")
